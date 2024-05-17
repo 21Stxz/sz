0dd13c51eb6872e6b3e71510173eb511aa86b073
21Stxz <ange.primiterra@labom2iformation.onmicrosoft.com>
2024-05-17 12:35:17 +0200

    Update run_bandit.sh

--- run_bandit.sh
+++ run_bandit.sh
@@ -1,37 +1,31 @@
-#!/bin/sh
-set +e
+#!/bin/bash
+
+set -e
 
 SRC_DIRECTORY="$(pwd)"
 REPORT_DIRECTORY="$SRC_DIRECTORY/report"
-BANDIT_IMAGE="secfigo/bandit:latest"
-IGNORE_FILE="/src/bandit.ignore"
 RESULT_FILE="$REPORT_DIRECTORY/banditResult.json"
+BANDIT_IMAGE="secfigo/bandit:latest"
 
-if [ ! -d "$REPORT_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
-    mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 755 "$REPORT_DIRECTORY"
-fi
+# Create the report directory if it doesn't exist
+mkdir -p "$REPORT_DIRECTORY"
+chmod -R 755 "$REPORT_DIRECTORY"
 
 # Pull the latest version of the Bandit Docker image
 echo "Pulling the latest version of the Bandit Docker image..."
 docker pull "$BANDIT_IMAGE"
 
-# Check if the Docker image was downloaded successfully
+# Run Bandit in a Docker container
+echo "Running Bandit tests..."
+docker run --rm \
+    --volume "$SRC_DIRECTORY":/src \
+    --volume "$REPORT_DIRECTORY":/report \
+    "$BANDIT_IMAGE" bandit -r -f json -o "$RESULT_FILE" /src
+
+# Check if Bandit command was successful
 if [ $? -eq 0 ]; then
-    # Run Bandit in a Docker container
-    echo "Running Bandit tests..."
-    docker run --rm \
-        --volume "$SRC_DIRECTORY":/src \
-        --volume "$REPORT_DIRECTORY":/report \
-        "$BANDIT_IMAGE" bandit -r -f json -o "$RESULT_FILE" /src
-    # Check if Bandit command was successful
-    if [ $? -eq 0 ]; then
-        echo "Bandit tests completed successfully."
-    else
-        echo "Bandit tests failed."
-    fi
+    echo "Bandit tests completed successfully."
 else
-    echo "Failed to download the Bandit Docker image."
+    echo "Bandit tests failed."
     exit 1
 fi
