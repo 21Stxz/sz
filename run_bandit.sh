e49850e49c4cf387bbe4aaa026ab119f2c89e266
21Stxz <ange.primiterra@labom2iformation.onmicrosoft.com>
2024-05-17 12:26:53 +0200

    Update run_bandit.sh

--- run_bandit.sh
+++ run_bandit.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 set +e
+
 SRC_DIRECTORY="$(pwd)"
 REPORT_DIRECTORY="$SRC_DIRECTORY/report"
+BANDIT_IMAGE="secfigo/bandit:latest"
 
 if [ ! -d "$REPORT_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
     mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
+    chmod -R 755 "$REPORT_DIRECTORY"
 fi
 
-# Make sure we are using the latest version
-docker pull secfigo/bandit:latest
+# Pull the latest version of the Bandit Docker image
+echo "Pulling the latest version of the Bandit Docker image..."
+docker pull "$BANDIT_IMAGE"
 
-docker run --rm \
-    --volume $(pwd):/src \
-    --volume "$REPORT_DIRECTORY":/report \
-    secfigo/bandit:latest
+# Check if the Docker image was downloaded successfully
+if [ $? -eq 0 ]; then
+    # Run Bandit in a Docker container
+    echo "Running Bandit tests..."
+    docker run --rm \
+        --volume "$SRC_DIRECTORY":/src \
+        --volume "$REPORT_DIRECTORY":/report \
+        "$BANDIT_IMAGE"
+else
+    echo "Failed to download the Bandit Docker image."
+    exit 1
+fi
